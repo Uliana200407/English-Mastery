06f10917256fdcd15c070f61d24b38aadca97cd1
Uliana200407 <114984015+Uliana200407@users.noreply.github.com>
2023-06-18 23:18:29 +0300

    Initial Commit

---
+++ English Mastery/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  English Mastery
+//
+//  Created by mac on 18.06.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
